8f0e49ee5736a38bc366c0d350f8cdab2c489fa5
Abhishakepeyyeti <138359125+Abhishakepeyyeti@users.noreply.github.com>
2025-06-08 12:27:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,13 +60,13 @@ if ingredients_list:
    
     # st.write(my_insert_stmt)
 
- time_to_insert = st.button('Submit Order')
-       
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()    
-
+    time_to_insert = st.button('Submit Order')
+    
     if ingredients_string:
         session.sql(my_insert_stmt).collect()
+  
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect() 
       
      
       
